ce73939f2ed33937cf219518bf433e8b7db26fff
Matt Lazewski <matthew.lazewski@gmail.com>
2020-05-04 23:28:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,9 +10,10 @@ def add_element_to_end_of_array(array, element)
   array=[]
   array << element
 end
-
-def add_element_to_start_of_array(array, element)
   
+def add_element_to_start_of_array(array, element)
+  array=[]
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
